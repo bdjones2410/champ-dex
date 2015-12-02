ea2f23db2b9835354a1989421c3466c76e978e50
Brandon Jones <bdjones2410@gmail.com>
2015-12-02 14:08:26 -0500

    won the spelling bee and got it working

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var port = process.env.PORT || 3001;
 var app = express();
-var http = require('http').Sever(app);
+var http = require('http').Server(app);
 
 app.use(express.static(__dirname + "/app"));
 
